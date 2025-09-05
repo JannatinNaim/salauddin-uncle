69e9b6b5dcec38c21d9caa8f0d911414f1db1026
Jannatin Naim <jannatinnaim@pondit.family>
2025-09-05 17:41:16 +0600

    add debug logs

--- src/main.ts
+++ src/main.ts
@@ -34,35 +34,45 @@ async function main() {
 
         switch (interaction.commandName) {
             case summonCommand.name: {
+                console.debug("Command Triggered:", summonCommand.name);
+
                 await interaction.reply({
                     // content: roleMention(process.env.RADIANT_ROLE_ID!) + " Radiants, assemble.",
                     content: roleMention(process.env.RADIANT_ROLE_ID!) + " Indian chudmu ajke, ay.",
                 });
 
                 const guild = client.guilds.resolve(process.env.DISCORD_GUILD_ID!)!;
+                console.debug("Triggered In Guild:", guild.name);
 
                 const role = (await guild.roles.fetch(process.env.RADIANT_ROLE_ID!))!;
+                console.debug("Mentioning Role:", role.name);
 
                 const audio = new AttachmentBuilder("assets/audio/summon.mp3");
 
-                await Promise.all(
-                    role.members
-                        .filter((member) => member.id !== interaction.user.id)
-                        .map((member) => {
-                            member.send({
-                                // content:
-                                //     "You're being summoned in " +
-                                //     channelMention(interaction.channelId) +
-                                //     " to play a match of Valorant.",
-                                content:
-                                    userMention(interaction.user.id) +
-                                    " tore Valorant khelte daktese ei channel e." +
-                                    channelMention(interaction.channelId) +
-                                    ". Taratari ay, otherwise guli bade shob lagay lamu.",
-                                files: [audio],
-                            });
-                        })
+                const membersToDM = role.members.filter((member) => member.id !== interaction.user.id);
+                console.debug(
+                    "Sending DMs:",
+                    membersToDM.map((member) => member.nickname || member.displayName).join(", ")
                 );
+
+                await Promise.all(
+                    membersToDM.map((member) => {
+                        return member.send({
+                            // content:
+                            //     "You're being summoned in " +
+                            //     channelMention(interaction.channelId) +
+                            //     " to play a match of Valorant.",
+                            content:
+                                userMention(interaction.user.id) +
+                                " tore Valorant khelte daktese ei channel e " +
+                                channelMention(interaction.channelId) +
+                                ". Taratari ay, otherwise guli bade shob lagay lamu.",
+                            files: [audio],
+                        });
+                    })
+                );
+
+                console.debug("Command Trigger Successful", summonCommand.name);
             }
         }
     });
